157b3216d5eb5e3c50883031d957e8dbe8ec6cdb
Ishaan Dey <ishaan1013@gmail.com>
2025-07-18 18:38:22 -0400

    fix chat top gradient

--- apps/frontend/components/chat/task.tsx
+++ apps/frontend/components/chat/task.tsx
@@ -238,7 +238,7 @@ export function TaskPageContent({
   const chatContent = (
     <div className="mx-auto flex w-full grow max-w-lg flex-col items-center relative z-0">
       {/* Todo: only show if not scrolled to the very top  */}
-      <div className="sticky -left-px w-[calc(100%+2px)] top-14 h-16 bg-gradient-to-b from-background via-background/60 to-transparent -translate-y-px pointer-events-none z-10" />
+      <div className="sticky -left-px w-[calc(100%+2px)] top-[calc(3rem+1px)] h-16 bg-gradient-to-b from-background via-background/60 to-transparent -translate-y-px pointer-events-none z-10" />
 
       <Messages messages={displayMessages} />
       <PromptForm
