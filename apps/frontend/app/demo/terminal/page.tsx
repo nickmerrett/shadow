712a3965905666a047a27cd2176cccf7de8aa453
Ishaan Dey <ishaan1013@gmail.com>
2025-07-20 03:28:00 -0400

    remove demo folder

--- apps/frontend/app/demo/terminal/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Terminal } from "@/components/agent-environment/terminal";
-
-export default function TerminalDemoPage() {
-  return (
-    <div className="h-screen">
-      <Terminal />
-    </div>
-  );
-}
