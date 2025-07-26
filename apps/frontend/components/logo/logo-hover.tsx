15667d35a1d8e828445d75a33b2a2a1d342b5c78
Ishaan Dey <ishaan1013@gmail.com>
2025-07-26 17:03:31 -0400

    fix logo scaling bug + classname color control

--- apps/frontend/components/logo/logo-hover.tsx
+++ apps/frontend/components/logo/logo-hover.tsx
@@ -3,20 +3,25 @@
 import { cn } from "@/lib/utils";
 import { useState } from "react";
 import "./logo-animation.css";
+import { ShadowLogoSvg } from "./logo-svg";
+import { ShadowLogoHoverSvg } from "./logo-hover-svg";
 
 const sizes = {
   sm: {
     width: 16,
+    animateWidth: 240,
     height: 16,
     className: "logo-sm",
   },
   md: {
     width: 20,
+    animateWidth: 300,
     height: 20,
     className: "logo-md",
   },
   lg: {
     width: 25,
+    animateWidth: 375,
     height: 25,
     className: "logo-lg",
   },
@@ -39,23 +44,29 @@ export function LogoHover({
     <div
       className={cn("overflow-hidden", className)}
       style={{ width: sizes[size].width, height: sizes[size].height }}
+      onMouseEnter={() => {
+        if (forceAnimate === undefined) {
+          setIsAnimating(true);
+        }
+      }}
+      onMouseLeave={() => {
+        if (forceAnimate === undefined) {
+          setIsAnimating(false);
+        }
+      }}
     >
       <div
-        className={cn("logo-container", sizes[size].className)}
-        role="img"
-        aria-label="Logo"
-        data-animate={shouldAnimate.toString()}
-        onMouseEnter={() => {
-          if (forceAnimate === undefined) {
-            setIsAnimating(true);
-          }
+        style={{
+          width: shouldAnimate ? sizes[size].animateWidth : sizes[size].width,
+          height: sizes[size].height,
         }}
-        onMouseLeave={() => {
-          if (forceAnimate === undefined) {
-            setIsAnimating(false);
-          }
-        }}
-      />
+      >
+        {shouldAnimate ? (
+          <ShadowLogoHoverSvg className={sizes[size].className} />
+        ) : (
+          <ShadowLogoSvg className={sizes[size].className} />
+        )}
+      </div>
     </div>
   );
 }
