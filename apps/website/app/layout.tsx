475cc11f5f33fe619b9cb69e75ef59d9dafc2885
Ishaan Dey <ishaan1013@gmail.com>
2025-08-01 05:30:43 -0400

    site

--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -56,6 +57,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -1,15 +1,9 @@
-import { LogoHover } from "@/components/logo/logo-hover";
+import HomeContent from "@/components/home";
+import { cookies } from "next/headers";
 
-export default function Home() {
-  return (
-    <div className="flex size-full items-center justify-center">
-      <div className="font-departureMono flex items-center gap-4 text-3xl font-medium tracking-tighter">
-        <LogoHover size="lg" forceAnimate />
-        Code with{" "}
-        <span className="text-muted-foreground inline-flex items-center gap-2">
-          Shadow
-        </span>
-      </div>
-    </div>
-  );
+export default async function Home() {
+  const cookieStore = await cookies();
+  const joinedWaitlist = cookieStore.get("joined-waitlist")?.value === "true";
+
+  return <HomeContent joinedWaitlist={joinedWaitlist} />;
 }
