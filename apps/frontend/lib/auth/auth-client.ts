4b556c1b5647e1b9c58c76004a228b60d721f546
Ishaan Dey <ishaan1013@gmail.com>
2025-08-04 05:17:25 -0400

    fix env var

--- apps/frontend/lib/auth/auth-client.ts
+++ apps/frontend/lib/auth/auth-client.ts
@@ -1,7 +1,16 @@
 import { createAuthClient } from "better-auth/react";
 
-const baseURL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+console.log(
+  "NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL",
+  process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+);
+console.log(
+  "NEXT_PUBLIC_VERCEL_BRANCH_URL",
+  process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+);
+
+const baseURL = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
   : "http://localhost:3000";
 
 export const authClient: ReturnType<typeof createAuthClient> = createAuthClient(
