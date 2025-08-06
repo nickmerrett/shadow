1817740c1c9a23f7884bb202654ec780814bb60f
Ishaan Dey <ishaan1013@gmail.com>
2025-08-06 18:10:21 -0400

    remove old files (incl. demo website)

--- apps/website/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import HomeContent from "@/components/home";
-import { cookies } from "next/headers";
-
-export default async function Home() {
-  const cookieStore = await cookies();
-  const joinedWaitlist = cookieStore.get("joined-waitlist")?.value === "true";
-
-  return <HomeContent joinedWaitlist={joinedWaitlist} />;
-}
